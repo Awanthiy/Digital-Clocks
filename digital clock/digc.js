fea750eb5b6270f2e32dc8720c9a1d7f9e22bd51
Awanthi Malawanage <121420903+Awanthiy@users.noreply.github.com>
2023-06-17 20:59:50 +0530

    Add files via upload

---
+++ digital clock/digc.js
@@ -0,0 +1,17 @@
+function displayTime() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+  
+    // Add leading zeros if necessary
+    hours = (hours < 10 ? "0" : "") + hours;
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+    seconds = (seconds < 10 ? "0" : "") + seconds;
+  
+    var timeString = hours + ":" + minutes + ":" + seconds;
+    document.getElementById("time").innerText = timeString;
+  }
+  
+  // Update the clock every second
+  setInterval(displayTime, 1000);
